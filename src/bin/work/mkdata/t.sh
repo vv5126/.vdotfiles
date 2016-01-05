ca09fabb70f91bb5895695f9d8e485a3effc1d54
Gao Wei <wei.gao@ingenic.com>
2016-01-05 20:36:19 +0800

    up t.sh

--- src/bin/work/mkdata/t.sh
+++ src/bin/work/mkdata/t.sh
@@ -6,27 +6,58 @@ function mk() {
 			gbs -v build -A mipsel --include-all --overwrite 2>&1|tee build.log
 		else
 			gbs -v build -A mipsel --include-all --overwrite $1 2>&1|tee build.log
+            if [ $? = 0 ]; then
+                cd -
+                . ./.project_info
+                [ ! -d "out" ] && mkdir out
+                cp -f $(find ~/GBS-ROOT -name $target_name) ./out 2>/dev/null
+                cd out
+                unrpm $target_name
+            fi
 		fi
 	fi
 }
 
-[ "$#" -gt 1 ] && {
+function init(){
+    local value=
+
+	forOS='tizen23'
+	repositories_type='git'
+	git_remote=$(git remote -v | head -2 | tail -1 | awk '{print $2}')
+	git_branch=$(git branch | grep '*' | awk '{print $2}')
+	# target_dir='$VGL_BOARDS/$forBOARD-$forOS-imgs'
+
+    while read line; do
+        if [[ "$line" =~ "Name:" ]]; then
+            value=$value${line##* }
+        fi
+        if [[ "$line" =~ "Version:" ]]; then
+            value=$value-${line##* }
+        fi
+        if [[ "$line" =~ "Release:" ]]; then
+            value=$value-${line##* }
+            break
+        fi
+    done < "$(find ./packaging -name *.spec)"
+    [ -n "$value" ] && target_name="$value.mipsel.rpm"
+
+	make_info_file
+	return 0
+}
+
+[ "$#" -ge 1 ] && {
 	case "$1" in
 	'init')
+		init
 		;;
 	'all')
 		mk all
 		;;
-	'')
-		if [ ! -d '.repo' ]; then
-	#		if [ ! -d '.git' ]; then
-				tizen_base_dir=`getdir_project | awk '{print $2}'`
-				tizen_module_dir=`getdir_git | awk '{print $2}'`
-				cd $tizen_base_dir; ./t.sh $tizen_module_dir
-	#		fi
-		fi
-		;;
 	*)
 		;;
 	esac
+} || {
+    tizen_base_dir=`getdir_project | awk '{print $2}'`
+    cd $tizen_base_dir
+    mk $OLDPWD
 }
