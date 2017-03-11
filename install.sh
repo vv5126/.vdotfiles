c4e92dacc46a80c2a940be9d15b2c3510f723988
Gao Wei <wei.gao@ingenic.com>
2017-03-11 19:46:43 +0800

    mv the dotfile to dots dir.

--- install.sh
+++ install.sh
@@ -1,31 +1,15 @@
 #!/bin/bash
 
-
 generate_link() {
-    origin_list="$PWD/config.list"
+    #origin_list="
     origin_dir="$HOME/.local/.origin"
 
     [ ! -d "$origin_dir" ] && mkdir -p "$origin_dir"
 
-    while read line; do
-        src_name=${line#*=}
-        dest_name=${line%%=*}
-        #	echo src_name = $src_name
-        #	echo dest_name = $dest_name
-        if [ -n "$src_name" -a -n "$dest_name" ]; then
-            if [ -w "$HOME/$dest_name" ]; then
-                cp -rf "$HOME/$dest_name" "$origin_dir"
-            fi
-            if [ -w "$PWD/src/$src_name" ]; then
-                [ -w "$HOME/$dest_name" ] && rm -r "$HOME/$dest_name"
-                ln -s "$PWD/src/$src_name" "$HOME/$dest_name"
-            fi
-        fi
-    done < "$origin_list"
-#	src="$PWD/src/$src_name"
-#	dest="$HOME/$dest_name"
-#	echo src = $src
-#	echo dest = $dest
+    for i in $(ls $PWD/src/dots -1); do
+        [ -w "$HOME/.$i" ] && mv -f "$HOME/.$i" "$origin_dir"
+        ln -s "$PWD/src/dots/$i" "$HOME/.$i"
+    done
 }
 
 sync_repo() {
--- remove.sh
+++ remove.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 
-origin_list="$PWD/config.list"
 origin_dir="$HOME/.local/.origin"
 
-while read line; do
-	src_name=${line#*=}
-	dest_name=${line%%=*}
-	if [ -w "$HOME/$dest_name" ]; then
-		rm -rf "$HOME/$dest_name"
-	fi
-	if [ -w "$origin_dir/$dest_name" ]; then
-		cp -r "$origin_dir/$dest_name" "$HOME"
-	fi
-done < "$origin_list"
+for i in $(ls $PWD/src/dots -1); do
+    [ -w "$HOME/.$i" ] && rm -f "$HOME/.$i"
+    [ -w "$origin_dir/.$i" ] && cp -r "$origin_dir/.$i" "$HOME"
+done
 
 rm $HOME/.bash_history 2>/dev/null
 rm $HOME/.cdlist 2>/dev/null
@@ -22,19 +15,3 @@ rm $HOME/.vim_mru_files 2>/dev/null
 rm -rf $HOME/.clipboard 2>/dev/null
 #rm -rf $HOME/.rm 2>/dev/null
 #rm -rf $HOME/.vdotfiles 2>/dev/null
-
-#	echo $line
-#	if [ -w "$HOME/$line" ]; then
-#		cp -r "$PWD/.origin/$line"
-#	echo src_name = $src_name
-#	echo dest_name = $dest_name
-#	if [ -n "$src_name" -a -n "$dest_name" ]; then
-#		src="$PWD/src/$src_name"
-#		dest="$HOME/$dest_name"
-#	echo src = $src
-#	echo dest = $dest
-#		if [ -w "$PWD/src/$src_name" ]; then
-#			ln -s "$PWD/src/$src_name" $test_dir/$dest_name
-#		fi
-#	fi
-
