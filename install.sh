c914c04c46b109856fcaef4c69fa6ac30a9ac08b
Gao Wei <wei.gao@ingenic.com>
2019-02-18 19:50:33 +0800

    fix more

--- install.sh
+++ install.sh
@@ -1,20 +1,37 @@
 #!/bin/bash
 
 generate_link() {
-    origin_dir="$HOME/.local/.olddot" # bak for old
+    local src dst dst_dir bak_dir="$HOME/.local/.olddot"
 
-    [ ! -d "$origin_dir" ] && mkdir -p "$origin_dir"
+    src="$1" dst="$2"
 
-    for i in $(ls $PWD/src/dots -1); do
-        [ -w "$HOME/.$i" ] && mv -f "$HOME/.$i" "$origin_dir"
-        ln -s "$PWD/src/dots/$i" "$HOME/.$i"
-    done
+    [ ! -e "$src" ] && return -1
+    [ -z "$dst" ] && dst="$HOME/.${src##*/}"
+    dst_dir="${dst%/*}"
+
+    [ ! -d "$bak_dir" ] && mkdir -p "$bak_dir"
+    [ -e "$dst" ] && mv -f "$dst" "$bak_dir"
+
+    [ ! -d "$dst_dir" ] && mkdir -p "$dst_dir"
+    ln -s "$src" "$dst"
 }
 
 # ======================== MAIN
 
-generate_link
+dotdir="$PWD/src/dots"
 
-source $HOME/.profile
+source "$dotdir/bashrc"
+
+mkdir -p $XDG_CONFIG_HOME $XDG_CACHE_HOME $XDG_DATA_HOME
+
+generate_link "$dotdir/vim"
+generate_link "$dotdir/vimrc"
+generate_link "$dotdir/bashrc"
+generate_link "$dotdir/bin"
+# generate_link "$dotdir/Xmodmap"
+# generate_link "$dotdir/zshrc"
+
+generate_link "$dotdir/bin/data/fonts.conf" "$XDG_CONFIG_HOME/fontconfig/fonts.conf"
+generate_link "$dotdir/bin/data/gitconfig"  "$XDG_CONFIG_HOME/git/config"
 
 echo " done!"
